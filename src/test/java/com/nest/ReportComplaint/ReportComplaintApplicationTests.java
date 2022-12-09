3b6222dc1dd765cc42618976900f6ce2e98b2537
delmashajan <delmashajan@gmail.com>
2022-12-09 14:18:01 +0530

    initial

---
+++ src/test/java/com/nest/ReportComplaint/ReportComplaintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.ReportComplaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportComplaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
